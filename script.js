79a82cb9e2bd1433bb79c7590646cf0de7fb3b39
DHARSHINI K <160514111+DHARSHINI-KAMARAJ@users.noreply.github.com>
2026-02-08 14:21:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -38,10 +38,11 @@ const sections = document.querySelectorAll("section");
 window.addEventListener("scroll", () => {
   sections.forEach(sec => {
     const top = window.scrollY;
-    const offset = sec.offsetTop - 450;
+    const offset = sec.offsetTop - window.innerHeight + 100;
     if (top >= offset) {
       sec.style.opacity = 1;
       sec.style.transform = "translateY(0)";
     }
   });
 });
+
