8dcbf055060944a7c2a2d9d4db3bb3b27a67808b
DHARSHINI K <160514111+DHARSHINI-KAMARAJ@users.noreply.github.com>
2026-02-08 11:28:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+const roles = [
+"Japanese N4 Certified",
+  "Software Developer",
+ "Bilingual Analyst",
+"Japanese Language Educator",
+  "International Tech Aspirant"
+];
+
+let roleIndex = 0;
+let charIndex = 0;
+const typing = document.querySelector(".typing");
+
+function type() {
+  if (charIndex < roles[roleIndex].length) {
+    typing.textContent += roles[roleIndex][charIndex];
+    charIndex++;
+    setTimeout(type, 100);
+  } else {
+    setTimeout(erase, 2000);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    typing.textContent = roles[roleIndex].substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(erase, 50);
+  } else {
+    roleIndex = (roleIndex + 1) % roles.length;
+    setTimeout(type, 500);
+  }
+}
+
+type();
+
+const sections = document.querySelectorAll("section");
+
+window.addEventListener("scroll", () => {
+  sections.forEach(sec => {
+    const top = window.scrollY;
+    const offset = sec.offsetTop - 450;
+    if (top >= offset) {
+      sec.style.opacity = 1;
+      sec.style.transform = "translateY(0)";
+    }
+  });
+});
